c437e04d9479090a59402a8c1faa07b1048e78bf
hahwul <hahwul@gmail.com>
2020-07-07 01:57:40 +0900

    update

--- cmd/crack.go
+++ cmd/crack.go
@@ -21,13 +21,14 @@ var crackCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(crackCmd)
 
-	crackCmd.PersistentFlags().StringVarP(&wordlist, "wordlist", "w", "", "wordlist file")
-	crackCmd.PersistentFlags().StringVar(&chars, "chars", "abcdefghijklmnopqrstuvwxyz0123456789", "char list (only bruteforce)")
-	crackCmd.PersistentFlags().IntVar(&lmin, "lmin", 1, "length of min (only bruteforce")
-	crackCmd.PersistentFlags().IntVar(&lmax, "lmax", 8, "length of max (only bruteforce")
+	crackCmd.PersistentFlags().StringVarP(&mode, "mode", "m", "dict", "cracking mode, you can use 'dict' or 'brute'")
+	crackCmd.PersistentFlags().StringVarP(&wordlist, "wordlist", "w", "", "wordlist file / only dictionary attack")
+	crackCmd.PersistentFlags().StringVar(&chars, "chars", "abcdefghijklmnopqrstuvwxyz0123456789", "char list / only bruteforce")
+	crackCmd.PersistentFlags().IntVar(&lmin, "lmin", 1, "length of min / only bruteforce")
+	crackCmd.PersistentFlags().IntVar(&lmax, "lmax", 8, "length of max / only bruteforce")
 
 	crackCmd.PersistentFlags().IntVarP(&conc, "concurrency", "c", 100, "number of concurrency")
-	crackCmd.Flags().BoolP("power", "t", false, "Help message for toggle")
+	crackCmd.Flags().BoolP("power", "t", false, "Used all CPU your computer")
 
 	// crackCmd.PersistentFlags().StringVar(&foo,"foo", "", "A help for foo")
 }
