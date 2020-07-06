cd4377aa0a6d47890ae011615ba23ca43469186d
hahwul <hahwul@gmail.com>
2020-07-07 00:00:21 +0900

    update

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -6,35 +6,47 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
-/*
-func main() {
-	fmt.Println("vim-go")
+// JWTencode is JWT interface for encode
+func JWTencode(claims map[string]interface{}, secret string) string {
+	var key = []byte(secret)
+	var jwtClaims = jwt.MapClaims(claims)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwtClaims)
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString(key)
+	_ = err
+	return tokenString
 }
-*/
 
-// encode is JWT interface for encode
-func JWTencode(string secret) {
+// JWTdecode is JWT interface for decode
+func JWTdecode(tokenString string) *jwt.Token {
+	// signature := false
 
-}
+	// Parse the token
+	var token *jwt.Token
+	var err error
+	parser := new(jwt.Parser)
+	// Figure out correct claims type
+	token, _, err = parser.ParseUnverified(tokenString, jwt.MapClaims{})
+	//token, _, err = parser.ParseUnverified(tokenString, &jwt.StandardClaims{})
 
-// decode is JWT interface for decode
-func JWTdecode(string tokenString) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return []byte("AllYourBase"), nil
-	})
-
-	if token.Valid {
-		fmt.Println("You look nice today")
-	} else if ve, ok := err.(*jwt.ValidationError); ok {
-		if ve.Errors&jwt.ValidationErrorMalformed != 0 {
-			fmt.Println("That's not even a token")
-		} else if ve.Errors&(jwt.ValidationErrorExpired|jwt.ValidationErrorNotValidYet) != 0 {
-			// Token is either expired or not active yet
-			fmt.Println("Timing is everything")
-		} else {
-			fmt.Println("Couldn't handle this token:", err)
-		}
-	} else {
-		fmt.Println("Couldn't handle this token:", err)
+	if err != nil {
+		fmt.Errorf("[%v] Invalid token", err)
+		return nil
 	}
+	return token
+}
+
+// JWTdecodeWithVerify is interface for decode with verify
+func JWTdecodeWithVerify(tokenString, secret string) (bool, *jwt.Token) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		//Make sure that the token method conform to "SigningMethodHMAC"
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return false, nil
+		}
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return false, nil
+	}
+	return true, token
 }
