bb9772fd40dbc947052f5d0cc99d1ca6312a17fd
hahwul <hahwul@gmail.com>
2020-07-07 00:37:12 +0900

    update

--- cmd/encode.go
+++ cmd/encode.go
@@ -8,6 +8,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var secret string
+
 // encodeCmd represents the encode command
 var encodeCmd = &cobra.Command{
 	Use:   "encode",
@@ -19,7 +21,7 @@ var encodeCmd = &cobra.Command{
 			if err := json.Unmarshal(mapInterface, &raw); err != nil {
 				// err
 			}
-			fmt.Println(jwtInterface.JWTencode(raw, "asdf"))
+			fmt.Println(jwtInterface.JWTencode(raw, secret))
 
 		} else {
 
@@ -29,14 +31,6 @@ var encodeCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(encodeCmd)
+	encodeCmd.PersistentFlags().StringVar(&secret, "secret", "", "secret key for JWT signature")
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// encodeCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// encodeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
