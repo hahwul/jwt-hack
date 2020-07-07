2fd2c4f9df76f314654a660bfbba1cf2eeb4a08c
hahwul <hahwul@gmail.com>
2020-07-08 00:35:17 +0900

    update

--- cmd/decode.go
+++ cmd/decode.go
@@ -5,6 +5,9 @@ import (
 
 	"github.com/dgrijalva/jwt-go"
 	jwtInterface "github.com/hahwul/jwt-hack/pkg/jwt"
+
+	//. "github.com/logrusorgru/aurora"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -13,12 +16,19 @@ var decodeCmd = &cobra.Command{
 	Use:   "decode [JWT Token]",
 	Short: "Decode JWT to JSON",
 	Run: func(cmd *cobra.Command, args []string) {
+		contextLogger := log.WithFields(log.Fields{
+			"common": "this is a common field",
+		})
 		if len(args) >= 1 {
 			var token *jwt.Token
 			token = jwtInterface.JWTdecode(args[0])
+			contextLogger.Info("RawData")
 			fmt.Println(token.Raw)
+			contextLogger.Info("Method")
 			fmt.Println(token.Method)
+			contextLogger.Info("Headers")
 			fmt.Println(token.Header)
+			contextLogger.Info("Claims")
 			fmt.Println(token.Claims)
 		} else {
 
